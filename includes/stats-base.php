4e9169ff3a5b1246754829550c912957dba89c10
Neil Ludlow <neil@shortdark.net>
2018-10-28 17:10:54 +0000

    Changed abstract class name for PSR-4

--- includes/stats-base.php
+++ includes/stats-base.php
@@ -1,6 +1,6 @@
 <?php
 
-abstract class statsBaseClass {
+abstract class StatsBaseClass {
 
 /**
  * ###########################
--- includes/stats-countries.php
+++ includes/stats-countries.php
@@ -10,7 +10,7 @@
  */
 
 
-class CountriesClass extends statsBaseClass
+class CountriesClass extends StatsBaseClass
 {
     /**
      * ########################
